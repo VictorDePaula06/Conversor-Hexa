0b0614fcf1c43000822d6d7c905ccba5fa13c1fe
João Victor de Paula <123420718+VictorDePaula06@users.noreply.github.com>
2024-12-24 22:35:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function convertToDecimal() {
     // Regex para verificar se o valor é um hexadecimal válido
     if (/^[0-9A-Fa-f]+$/.test(hexInput)) {
         const decimalValue = parseInt(hexInput, 16); // Converte de hexadecimal para decimal
-        resultadoElement.textContent = `Resultado: ${decimalValue}`; // Exibe o resultado
+        resultadoElement.textContent = `${decimalValue}` ; // Exibe o resultado
         resultadoElement.style.color = "rgb(88, 171, 243)"; // Define uma cor para o texto válido
 
         // Feedback visual no input
@@ -23,4 +23,30 @@ function convertToDecimal() {
         document.getElementById('hexInput').classList.remove('success');
         document.getElementById('hexInput').classList.add('error');
     }
+
+
 }
+
+
+
+document.getElementById('copyButton').addEventListener('click', () => {
+    const resultadoText = document.getElementById('resultado').textContent;
+    navigator.clipboard.writeText(resultadoText.replace('Resultado: ', ''));
+    alert('Resultado copiado!');
+});
+
+function playSound() {
+    const sound = document.getElementById('clickSound');
+    sound.play();
+}
+
+
+
+
+
+
+
+
+
+
+
